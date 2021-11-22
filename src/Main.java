0b07213ca951016a23559a20680fdeb720accfa8
PiotrWojtalewicz <piowojt19@gmail.com>
2021-11-22 19:01:35 +0100

    zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,23 @@ public class Main {
 
         /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
           Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indeksu (podać nr indesu z uczelni)
-          to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
-        */
+          to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik) */
+        System.out.println("Zadanie 1");
+        int i = 1000;
+        System.out.println("\n");
+        do{
+            System.out.println("i: " + i);
+            if( i % 27 ==0) {
+                System.out.println("Liczba " + i + " jest podzielna przez 27 " );
+            }
+        } while (--i > 0);
 
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
           przeiterować tablicę po elementach i wyswietlić, użyć pętli while
-        b) jak wyżej tylko dla tablicy typu double[],sss
-        c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
+        b) jak wyżej tylko dla tablicy typu double[],
+        c) jak wyżej dla tablicy String[], przeliterować i wyświetlić od ostatniego elementu
         */
 
         /* zad.3
