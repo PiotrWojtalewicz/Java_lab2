734a5f6f350aaa1ada5b51b0b25d16713d9fdc35
PiotrWojtalewicz <piowojt19@gmail.com>
2021-11-29 22:03:08 +0100

    zadanie 4

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,26 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
-
-
+        System.out.println("zadanie 1a");
+        for (int a = 1; a <= 100; a++) {
+            if (a == 15) {
+                break;
+            }
+            System.out.println(a);
+        }
+        System.out.println("zadanie 1b");
+        for (int b = 100; b >= 0; b--) {
+            if (b == 99) {
+                continue;
+            }
+            System.out.println(b);
+        }
+        System.out.println("zadanie 1c");
+        for (int c = 5; c <= 20; c++) {
+            if (c == 7) {
+                return;
+            }
+            System.out.println(c);
+        }
     }
 }
